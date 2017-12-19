329a5ab667021a5c7e1f5edb450d7819969a85fb
0604Revathi <34621637+0604Revathi@users.noreply.github.com>
2017-12-19 09:01:21 -0800

    Add files via upload

---
+++ formula.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class formula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter C value");
+		int C=sc.nextInt();
+		int F = (C*2)+30;
+		System.out.println(F);
+		
+	}
+
+}
---
+++ number.java
@@ -0,0 +1,27 @@
+
+public class number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+for(int i=1;i<100;i++)
+{
+
+		
+	if(i%3==0) 
+	{
+		System.out.println("Fizz");
+	}
+	
+	else if(i%5==0)
+	{
+		System.out.println("Buzz");
+	}
+	else
+	{
+		System.out.println(i);
+	}
+}
+	}
+
+}
