e1dcd80b851675b12bcedaf1f515bc45d6c423db
0604Revathi <34621637+0604Revathi@users.noreply.github.com>
2017-12-21 15:58:58 -0800

    Add files via upload

---
+++ lengthconv.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class lengthconv {
+	public static double metersToFeet()
+	{
+		double result = 0;
+		int meters=25;
+		result=meters/0.3048;
+		retrun result;
+	}
+	public static double feetsTOInches()
+	{
+		double result = 0;
+		double feet = metersToFeet();
+		result = feet*12;
+		retrun result;
+	}
+	public static double inchesToMeters()
+	{
+		double result=0;
+		double inches=feetsTOInches();
+		result=inches*0.0245;
+		retrun result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The meters to feets conversion result is"+metersToFeet());
+		System.out.println("The meters to feets conversion result is"+feetsTOInches());
+		System.out.println("The meters to feets conversion result is"+inchesToMeters());
+
+	}
+
+}
