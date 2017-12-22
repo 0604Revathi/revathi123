96a68bc618da2c5eb8f91939dd1bbbda99b61340
0604Revathi <34621637+0604Revathi@users.noreply.github.com>
2017-12-22 07:10:08 -0800

    Add files via upload

--- lengthconv.java
+++ lengthconv.java
@@ -5,21 +5,21 @@ public class lengthconv {
 		double result = 0;
 		int meters=25;
 		result=meters/0.3048;
-		retrun result;
+		return result;
 	}
 	public static double feetsTOInches()
 	{
 		double result = 0;
 		double feet = metersToFeet();
 		result = feet*12;
-		retrun result;
+		return result;
 	}
 	public static double inchesToMeters()
 	{
 		double result=0;
 		double inches=feetsTOInches();
 		result=inches*0.0245;
-		retrun result;
+		return result;
 	}
 
 	public static void main(String[] args) {
